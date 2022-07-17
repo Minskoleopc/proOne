02be566efac53065ab5e12cd08b09d727594ae42
Minskoleopc <minskoleopc@gmail.com>
2022-07-17 21:13:57 +0530

    roll dice

--- script.js
+++ script.js
@@ -12,69 +12,75 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing
 
-let scores, currentScore , activePlayer ,playing
 
-// reset values 
+// // reset values 
 
-let init = function(){
+// let init = function () {
+//     scores = [0, 0]
+//     currentScore = 0
+//     activePlayer = 0
+//     playing = true
 
-    scores = [0,0]
+//     diceEl.style.display = "none"
+//     // element  text reset to zero
+//     score0El.textContent = 0
+//     score1El.textContent = 0
+//     current0El.textContent = 0
+//     current1El.textContent = 0
+//     document.getElementById(`score--0`).classList.remove('player--winner')
+//     document.getElementById(`score--1`).classList.remove('player--winner')
+//     document.getElementById(`score--0`).classList.remove('player--active')
+//     document.getElementById(`score--1`).classList.remove('player--active')
+
+// }
+
+// init()
+
+
+
+
+// // Roll the dice functionality 
+function switchPlayer() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0
     currentScore = 0
-    activePlayer = 0
-    playing = true
-
-    diceEl.style.display = "none"
-    // element  text reset to zero
-    score0El.textContent = 0
-    score1El.textContent = 0
-    current0El.textContent = 0
-    current1El.textContent = 0
-
-
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
 }
 
-init()
+// btnRoll.addEventListener('click', function () {
+//     // generating the random number 
+//     if (playing) {
+//         let randomNumber = Math.floor(Math.random() * 6) + 1
+//         console.log(randomNumber)
+//         diceEl.style.display = "block"
+//         diceEl.src = `dice-${randomNumber}.png`
+//         if (randomNumber != 1) {
+//             currentScore = currentScore + randomNumber  // 5
+//             document.getElementById(`current--${activePlayer}`).textContent = currentScore
+//         }
+//         else {
+//             switchPlayer()
+//         }
+//     }
+// })
 
+// // click on hold button
+// btnHold.addEventListener('click', function () {
+//     if (playing) {
+//         scores[activePlayer] = currentScore + scores[activePlayer]
+//         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+//         if (scores[activePlayer] >= 20) {
+//             playing = false
+//             document.getElementById(`score--${activePlayer}`).classList.add('player--winner')
+//             diceEl.style.display = "none"
+//         }
+//         else {
+//             switchPlayer()
+//         }
+//     }
+// })
 
-
-
-// Roll the dice functionality 
-
-function switchPlayer(){
-
-    document.getElementById(`current--${activePlayer}`).textContent   = 0
-    currentScore = 0 
-    activePlayer = activePlayer === 0 ?1:0
-
-
-    //if(activePlayer == 0){
-    //     activePlayer = 1
-    // }
-    // else {
-    //     activePlayer = 0
-    // }
-
-}
-
-
-
-
-
-
-
-btnRoll.addEventListener('click',function(){
-    // generating the random number 
-    let randomNumber = Math.floor(Math.random() * 6) + 1
-    console.log(randomNumber)
-    diceEl.style.display = "block"
-    diceEl.src= `dice-${randomNumber}.png`
-
-    if(randomNumber != 1){
-        currentScore = currentScore + randomNumber  // 5
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore
-    }
-    else {
-        switchPlayer()
-    }
-})
+// document.querySelector('.btn--new').addEventListener('click', init)
